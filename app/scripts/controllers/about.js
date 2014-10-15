a107bc226b0e1518f96b6df7dd9b0821702f02ab
climboid <oscarvillarreal14@gmail.com>
2014-10-15 16:18:27 -0600

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name d3jstestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the d3jstestApp
+ */
+angular.module('d3jstestApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name d3jstestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the d3jstestApp
+ */
+angular.module('d3jstestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
